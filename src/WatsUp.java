98c42d2bc18feb565705e029f2305a8222a78472
Josh <joshgetter@comcast.net>
2016-09-14 12:48:56 -0400

    Switched to maven for dependencies, and added watson wrapper to maven.

--- src/WatsUp.java
+++ src/WatsUp.java
@@ -1,9 +1,13 @@
-
+import com.ibm.watson.developer_cloud.*;
+import com.ibm.watson.developer_cloud.text_to_speech.v1.TextToSpeech;
+import com.ibm.watson.developer_cloud.text_to_speech.v1.model.Voice;
 public class WatsUp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Wats Up!");
+		TextToSpeech textToSpeech = new TextToSpeech();
+		
 	}
 
 }
