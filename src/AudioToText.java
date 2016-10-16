408eb87d2e269c87177a0209f474731f383211a3
Charles Billingsley <charlesebillingsley@yahoo.com>
2016-10-16 13:06:20 -0400

    Committing my goods

--- src/AudioToText.java
+++ src/AudioToText.java
@@ -1,8 +1,5 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.concurrent.Callable;
-import java.util.concurrent.Future;
 
 import com.ibm.watson.developer_cloud.http.HttpMediaType;
 import com.ibm.watson.developer_cloud.speech_to_text.v1.RecognizeOptions;
@@ -11,43 +8,78 @@ import com.ibm.watson.developer_cloud.speech_to_text.v1.model.SpeechModel;
 import com.ibm.watson.developer_cloud.speech_to_text.v1.model.SpeechResults;
 import com.ibm.watson.developer_cloud.speech_to_text.v1.websocket.BaseRecognizeDelegate;
 
+/**
+ * Class that takes the inputed audio and sends it off to Watson to be changed
+ * to text.
+ * 
+ * @author Josh Getter
+ * @author Charles Billingsley
+ * @author Brent Willman
+ *
+ */
 public class AudioToText implements Callable {
-String toReturn;
-CaptureAudio capture = new CaptureAudio();
+	
+	/**
+	 * The string that was transcribed by Watson.
+	 */
+	private String transcribedPhrase;
+	
+	/**
+	 * The audio as it's captured.
+	 */
+	CaptureAudio capture = new CaptureAudio();
+
 	@Override
-	public String call() throws Exception {
-		// TODO Auto-generated method stub
-		//String toReturn;
+	public final String call() throws Exception {
+		// String toReturn;
 		SpeechToText service = new SpeechToText();
-		service.setUsernameAndPassword("95c52d86-c897-4870-99bc-e1bcaa6b25d5", "dA2xxyWJmgHy");
+		service.setUsernameAndPassword("95c52d86-c897-4870-99bc-e1bcaa6b25d5",
+				"dA2xxyWJmgHy");
 		List<SpeechModel> models = service.getModels();
 		System.out.println(models);
-		RecognizeOptions options = new RecognizeOptions().contentType(HttpMediaType.AUDIO_RAW + "; rate=16000")
-				  .continuous(true).interimResults(false);
-		
-				BaseRecognizeDelegate delegate = new BaseRecognizeDelegate() {
-				    @Override
-				    public void onMessage(SpeechResults speechResults) {
-				      System.out.println(speechResults);
-				      toReturn = speechResults.toString();
-				      
-				    }
-				    	
-				    @Override
-				    public void onError(Exception e) {
-				      e.printStackTrace();
-				    }
-				  };
+		RecognizeOptions options = new RecognizeOptions()
+				.contentType(HttpMediaType.AUDIO_RAW + "; rate=16000")
+				.continuous(true).interimResults(false);
 
+		BaseRecognizeDelegate delegate = new BaseRecognizeDelegate() {
+			@Override
+			public void onMessage(final SpeechResults speechResults) {
+				System.out.println(speechResults);
+				setTranscribedPhrase(speechResults.toString());
+			}
 
-		service.recognizeUsingWebSockets(capture.getStream(), options, delegate);
-		//SpeechResults transcript = service.recognize(capture.getStream(), options);
-		return toReturn;
+			@Override
+			public void onError(final Exception e) {
+				e.printStackTrace();
+			}
+		};
+
+		service.recognizeUsingWebSockets(capture.getStream(),
+				options, delegate);
+		// SpeechResults transcript = service.recognize(capture.getStream(),
+		// options);
+		return getTranscribedPhrase();
 	}
-	public void endStream(){
+
+	/**
+	 * Stops the stream.
+	 */
+	public final void endStream() {
 		capture.shutDown(null);
 	}
 
+	/**
+	 * @return the transcribedPhrase
+	 */
+	public final String getTranscribedPhrase() {
+		return transcribedPhrase;
+	}
+
+	/**
+	 * @param pTranscribedPhrase the transcribedPhrase to set
+	 */
+	public final void setTranscribedPhrase(final String pTranscribedPhrase) {
+		this.transcribedPhrase = pTranscribedPhrase;
+	}
+
 }
-
-
